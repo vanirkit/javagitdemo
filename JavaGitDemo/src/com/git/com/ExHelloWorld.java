74f956150a86d076efae097477af0defbe915460
Dell <Dell@Vani>
2024-09-05 09:38:33 -0700

    javaGitdemo

---
+++ JavaGitDemo/src/com/git/com/ExHelloWorld.java
@@ -0,0 +1,11 @@
+package com.git.com;
+
+public class ExHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
